d0993d1abee08b6b536470451e85cc44b52fd05d
Martin Johan Stave <marstave@hotmail.com>
2018-10-03 19:18:18 +0200

    virkelig

--- WpfWebDemo/WpfWebDemo/MainWindow.xaml.cs
+++ WpfWebDemo/WpfWebDemo/MainWindow.xaml.cs
@@ -62,7 +62,7 @@ namespace WpfWebDemo
 
                         <script type='text/babel'>
                             function test(){
-                               alert('ok fra func')
+                               alert('ok fra func virkelig')
                             }
                         </script>
                         <style>
